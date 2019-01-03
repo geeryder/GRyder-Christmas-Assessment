a2409181fee49150b1edc6341f73e10f23262b57
Georgina Ryder <Georgina.Ryder@nology.io>
2019-01-03 20:28:54 +0000

    error messages on register done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { ThankyouLettersComponent } from './thankyou-letters/thankyou-letters.component';
+import { NavComponent } from './nav/nav.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
@@ -28,7 +29,8 @@ const appRoutes: Routes = [
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    ThankyouLettersComponent
+    ThankyouLettersComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
